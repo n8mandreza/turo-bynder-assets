837d6b475e68290974d3bc666f0af2a0f5aebb52
Nathan Mandreza <nymandreza@gmail.com>
2024-05-28 17:25:17 -0700

    Update useEffect to run only when isClient

--- app/routes/_auth.callback.tsx
+++ app/routes/_auth.callback.tsx
@@ -17,7 +17,7 @@ export default function CallbackRoute() {
     const urlParams = new URLSearchParams(window.location.search)
     const urlCode = urlParams.get('code')
 
-    if (urlCode && !authCode) {
+    if (urlCode && !authCode && isClient) {
       setAuthCode(urlCode)
 
       // Perform POST request to the API token endpoint here
