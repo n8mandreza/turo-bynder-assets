cb8fe958c8d71195b60fd83933182ba152bf8420
Nathan Mandreza <nymandreza@gmail.com>
2024-06-07 20:04:08 -0700

    Add authorization header to asset fetch response

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -20,7 +20,6 @@ interface AssetType {
 
 export default function Index() {
   const {accessToken, refreshToken} = useAuthData();
-  const [isClient, setIsClient] = useState(false);
 
   const [query, setQuery] = useState('')
   const [results, setResults] = useState(null)
@@ -35,20 +34,41 @@ export default function Index() {
   async function fetchAssets(query: string, page: number) {
     const assetsEndpoint = `https://assets.turo.com/api/v4/media/?keyword=${query}&page=${page}&total=1`
 
-    return await fetch(assetsEndpoint)
-      .then(async (response) => {
-        const results = await response.json();
-        // console.log(results);
-        setResultsCount(results.total.count);
-        
-        return results.media.map((result: AssetType) => {
-          return {
-            name: result.name,
-            id: result.id,
-            thumbnail: result.thumbnails.webimage
-          };
-        });
-      })
+    if (!accessToken) {
+      console.error('No access token available.')
+      return;
+    }
+
+    // Fetch assets with Authorization header
+    try {
+      const response = await fetch(assetsEndpoint, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      // Check if the server response is not ok then throw an error
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const results = await response.json();
+      console.log('Fetch response', results);
+
+      setResultsCount(results.total.count);
+
+      // Process and return the transformed array of results
+      return results.media.map((result: AssetType) => ({
+        name: result.name,
+        id: result.id,
+        thumbnail: result.thumbnails.webimage
+      }));
+    } catch (error) {
+      console.error('Error fetching assets:', error);
+      throw error;
+    }
   }
 
   function handleSearch(event: React.FormEvent<HTMLFormElement>) {
@@ -58,7 +78,7 @@ export default function Index() {
     fetchAssets(query, newPage)
       .then(results => {
         setResults(results)
-        // console.log(query)
+        console.log('Query keyword:', query)
       })
   }
 
