e3d5416cba8275e7efb6fc6319bd1b69d6b0c7df
Nathan Mandreza <nymandreza@gmail.com>
2024-06-09 23:47:54 -0700

    Fix typo

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -9,7 +9,7 @@ export interface SearchInputProps {
 
 export default function SearchInput({ id, label, placeholder, onInput }: SearchInputProps) {
     return (
-        <div className="flex item-center gap-3 w-full px-3 py-2 rounded-lg backdrop-blur-xl surface-material drop-shadow-xl focus:interactive-focus below-m">
+        <div className="flex items-center gap-3 w-full px-3 py-2 rounded-lg backdrop-blur-xl surface-material drop-shadow-xl focus:interactive-focus below-m">
             <MagnifyingGlass />
 
             <input id={id} type="text" aria-label={label} placeholder={placeholder} onInput={onInput} className="w-full bg-transparent text-base placeholder:text-02 focus-visible:outline-none" />
