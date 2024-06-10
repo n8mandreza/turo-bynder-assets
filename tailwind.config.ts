28e44b74c3184a4d8d5d84b0f7768215a0ba1873
Nathan Mandreza <nymandreza@gmail.com>
2024-06-10 11:42:54 -0700

    Fix drop shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -166,8 +166,8 @@ export default {
     }),
     dropShadow: {
       'md': [
-        '0 4px 8px rgba(18, 18, 20, 0.2)',
-        '0 0 2px rgba(18, 18, 20, 0.1)'
+        '0 4px 8px rgba(18, 18, 20, 0.15)',
+        '0 0 2px rgba(18, 18, 20, 0.05)'
       ],
       'md-dark': [
         '0 4px 8px rgba(0, 0, 0, 0.2)',
