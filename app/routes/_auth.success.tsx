b98fc5c1fd4c4f56aa5659c40dcb1a1e15f85940
Nathan Mandreza <nymandreza@gmail.com>
2024-07-10 01:03:59 -0700

    Update success page styles

--- app/routes/_auth.success.tsx
+++ app/routes/_auth.success.tsx
@@ -3,10 +3,15 @@ import SpotThumbsUpSuccess from "~/illustrations/SpotThumbsUpSuccess";
 
 export default function SuccessRoute() {
     return (
-        <div className="flex flex-col gap-4 items-center justify-center w-screen h-screen">
-            <SpotIllustration spotIllustration={<SpotThumbsUpSuccess />} />
-            <h1 className="text-xl">You've successfully logged into Bynder</h1>
-            <p>You may now close this window and return to Figma.</p>
+        <div className="flex flex-col items-center justify-center w-screen h-screen">
+            <div className="surface-success rounded-2xl flex items-center gap-4 py-3 px-6">
+                <SpotIllustration spotIllustration={<SpotThumbsUpSuccess />} />
+
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-2xl font-medium">You've successfully logged into Bynder</h1>
+                    <p>You may now close this window and return to Figma.</p>
+                </div>
+            </div>
         </div>
     )
 }
