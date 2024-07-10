49c229696596419eddaefd3060da13bd8202d8cc
Nathan Mandreza <nymandreza@gmail.com>
2024-07-10 00:36:43 -0700

    Update success view

--- app/components/ProgressIndicator.tsx
+++ app/components/ProgressIndicator.tsx
@@ -1,7 +1,7 @@
 export default function ProgressIndicator() {
     return (
         <div className="spinner-container">
-            <div className="spinner"></div>
+            <div className="spinner stroke-01 border-l-accent"></div>
         </div>
     )
 }
