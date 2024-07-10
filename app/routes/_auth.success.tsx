3ce65aaf09441690df2ec214c2121da101170573
Nathan Mandreza <nymandreza@gmail.com>
2024-07-10 01:54:03 -0700

    Fix success text spacing

--- app/routes/_auth.success.tsx
+++ app/routes/_auth.success.tsx
@@ -7,7 +7,7 @@ export default function SuccessRoute() {
             <div className="surface-success rounded-2xl flex items-center gap-4 py-3 px-6">
                 <SpotIllustration spotIllustration={<SpotThumbsUpSuccess />} />
 
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
                     <h1 className="text-2xl font-medium">You've successfully logged into Bynder</h1>
                     <p>You may now close this window and return to Figma.</p>
                 </div>
