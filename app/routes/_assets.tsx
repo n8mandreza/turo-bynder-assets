43062fe232d3610ed0f956e174a069ea1111b2bf
Nathan Mandreza <nymandreza@gmail.com>
2024-07-10 12:10:46 -0700

    Try moving search to separate route

---
+++ app/routes/_assets.tsx
@@ -0,0 +1,50 @@
+import { Outlet, useNavigate } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { useAuthData } from "~/AuthContext";
+
+export default function AssetsLayout() {
+    const { accessToken, saveAccessToken, resetAccessToken } = useAuthData();
+
+    const [hasCheckedToken, setHasCheckedToken] = useState(false);
+    const navigate = useNavigate()
+
+    // On load, check if there's an existing access token
+    const checkAccessToken = (event: MessageEvent) => {
+        console.log("Message received by UI:", event.data)
+
+        if (event?.data?.pluginMessage?.message === 'GET_EXISTING_ACCESS_TOKEN') {
+            const accessToken = event?.data?.pluginMessage?.accessToken
+            // Check if that token works
+            // and save it to use with network requests
+            console.log('Access token from plugin message', accessToken)
+            saveAccessToken(accessToken)
+        }
+
+        setHasCheckedToken(true);
+    }
+
+    useEffect(() => {
+        window.addEventListener('message', checkAccessToken);
+        console.log('Added checkAccessToken event listener')
+
+        return () => {
+            window.removeEventListener('message', checkAccessToken)
+            console.log('Removed checkAccessToken event listener')
+        }
+    }, [])
+
+    // Navigate to _auth.login if accessToken is null
+    useEffect(() => {
+        if (hasCheckedToken && !accessToken) {
+            navigate('/login');
+        } else {
+            navigate('/search');
+        }
+    }, [accessToken, hasCheckedToken, navigate]);
+    
+    return (
+        <div className="flex items-center justify-center w-screen h-screen">
+            <Outlet />
+        </div>
+    )
+}
--- app/routes/collections.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "@remix-run/react";
-
-export default function CollectionsLayout() {
-    return (
-        <div className="flex items-center justify-center w-screen h-screen">
-            <Outlet/>
-        </div>
-    )
-}
