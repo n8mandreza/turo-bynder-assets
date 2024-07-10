27630917eec0a8b875edf82c0de5ad82ca46d4e0
Nathan Mandreza <nymandreza@gmail.com>
2024-07-10 00:48:14 -0700

    Add favicon; fix SpotIllustration styles

--- app/components/SpotIllustration.tsx
+++ app/components/SpotIllustration.tsx
@@ -4,8 +4,8 @@ interface SpotIllustrationProps {
 
 export default function SpotIllustration({ spotIllustration }: SpotIllustrationProps) {
     return (
-        <div className="w-16 h-16 relative">
-            <div className="opacity-60 rounded-full spot-bg-success"></div>
+        <div className="w-16 h-16 relative flex items-center justify-center">
+            <div className="opacity-60 rounded-full spot-bg-success w-[90%] h-[90%]"></div>
 
             <div className="z-10 absolute top-0 left-0 right-0 bottom-0">
                 {spotIllustration}
