3ac71d9545cc7780d190d91c90c646b936f947d4
Nathan Mandreza <nymandreza@gmail.com>
2024-07-12 12:41:20 -0700

    Fix NavBar text style

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,9 +2,9 @@ import { Link } from "@remix-run/react";
 
 export default function NavBar() {
     return (
-        <div className="flex fixed surface-sticky top-0 left-0 right-0 z-50 gap-3 px-4 py-2 border-b stroke-01">
+        <div className="flex fixed surface-sticky top-0 left-0 right-0 z-50 gap-3 px-4 py-2 border-b stroke-01 text-sm">
             <Link to="/search">
-                <p className="text-sm hover:opacity-60">Search</p>
+                <p className="hover:opacity-60">Search</p>
             </Link>
 
             <p className="text-02 pointer-events-none">
