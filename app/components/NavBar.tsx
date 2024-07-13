686d899e14c6a62b96c8fa79134dc9ee4c7f6f1e
Nathan Mandreza <nymandreza@gmail.com>
2024-07-13 12:44:27 -0700

    fix styles

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function NavBar() {
     return (
-        <div className="flex fixed surface-sticky top-0 left-0 right-0 z-50 gap-3 px-4 py-2 border-b stroke-01 text-sm">
+        <div className="flex sticky surface-sticky w-full z-50 gap-3 px-4 py-2 border-b stroke-01 text-sm">
             <Link to="/search">
                 <p className="hover:opacity-60">Search</p>
             </Link>
--- app/routes/_assets.tsx
+++ app/routes/_assets.tsx
@@ -1,50 +1,9 @@
-import { Link, Outlet, useNavigate } from "@remix-run/react";
-import { useEffect, useState } from "react";
-import { useAuthData } from "~/AuthContext";
+import { Outlet } from "@remix-run/react";
 import NavBar from "~/components/NavBar";
 
 export default function AssetsLayout() {
-    // const { accessToken, saveAccessToken, resetAccessToken } = useAuthData();
-    // const navigate = useNavigate()
-
-    // const [hasCheckedToken, setHasCheckedToken] = useState(false);
-
-    // // On load, check if there's an existing access token
-    // const checkAccessToken = (event: MessageEvent) => {
-    //     console.log("Message received by UI:", event.data)
-
-    //     if (event?.data?.pluginMessage?.message === 'GET_EXISTING_ACCESS_TOKEN') {
-    //         const accessToken = event?.data?.pluginMessage?.accessToken
-    //         // Check if that token works
-    //         // and save it to use with network requests
-    //         console.log('Access token from plugin message', accessToken)
-    //         saveAccessToken(accessToken)
-    //     }
-
-    //     setHasCheckedToken(true);
-    // }
-
-    // useEffect(() => {
-    //     window.addEventListener('message', checkAccessToken);
-    //     console.log('Added checkAccessToken event listener')
-
-    //     return () => {
-    //         window.removeEventListener('message', checkAccessToken)
-    //         console.log('Removed checkAccessToken event listener')
-    //     }
-    // }, [])
-
-    // // Navigate to _auth.login if accessToken is null
-    // useEffect(() => {
-    //     if (hasCheckedToken && !accessToken) {
-    //         navigate('/login');
-    //     } else {
-    //         navigate('/search');
-    //     }
-    // }, [accessToken, hasCheckedToken, navigate]);
-    
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen pt-8">
+        <div className="flex flex-col items-center justify-center w-screen h-screen">
             <NavBar />
             
             <Outlet />
