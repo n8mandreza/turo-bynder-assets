35faa3f5b7e8c8bc0ab9bb0ec31aea8388361c98
Nathan Mandreza <nymandreza@gmail.com>
2024-07-14 16:07:18 -0700

    Add collection dynamic route

---
+++ app/routes/_assets.collections.$id.tsx
@@ -0,0 +1,71 @@
+import { useNavigate, useParams } from "@remix-run/react";
+import { useEffect, useState } from "react"
+import { useAuthData } from "~/AuthContext";
+
+export default function CollectionRoute() {
+    const { accessToken, resetAccessToken } = useAuthData();
+    const navigate = useNavigate()
+    const { id } = useParams()
+
+    const [assets, setAssets] = useState<any>([])
+
+    async function fetchCollectionMedia(id: string) {
+        const collectionEndpoint = `https://assets.turo.com/api/v4/collections/${id}/media`
+
+        if (!accessToken) {
+            console.error('No access token available.')
+            navigate('/login')
+        }
+
+        try {
+            const response = await fetch(collectionEndpoint, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            if (!response.ok) {
+                if (response.status === 401) {
+                    resetAccessToken()
+                }
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+
+            const results = await response.json()
+            console.log('Collection fetch response:', results)
+
+            return results.collections.map((result: any) => ({
+                id: result.id
+            }))
+        } catch (error) {
+            console.error('Error fetching assets:', error)
+            throw error
+        }
+    }
+
+    useEffect(() => {
+        if (id) {
+            fetchCollectionMedia(id).then((fetchedResults) => {
+                setAssets(fetchedResults)
+            })
+        }
+    }, [])
+
+    return (
+        <div className="flex flex-col gap-4 overflow-scroll w-full h-full">
+            {assets && assets.length > 0 ? (
+                <div className="flex flex-col p-4 gap-4">
+                    {assets.map((asset: any) => (
+                        <p>{asset.id}</p>
+                    ))}
+                </div>
+            ) : (
+                <div className="flex flex-col gap-3 w-full h-full items-center justify-center">
+                    <p className="px-8 text-center">Unable to retrieve assets or no image assets in collection.</p>
+                </div>
+            )}
+        </div>
+    )
+}
--- app/routes/_assets.collections.tsx
+++ app/routes/_assets.collections.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "@remix-run/react";
+import { Link, useNavigate } from "@remix-run/react";
 import { useEffect, useState } from "react"
 import { useAuthData } from "~/AuthContext";
 import Pagination from "~/components/Pagination"
@@ -89,17 +89,19 @@ export default function CollectionsRoute() {
 
             <div className="grid grid-cols-2 gap-4">
               {collections.map((collection) => (
-                <div id={collection.id} className="flex flex-col gap-1">
-                  <div className="overflow-hidden rounded-lg relative aspect-square">
-                    <div className="absolute top-1 left-1 surface-material px-2 py-1 rounded-md">
-                      <p className="text-xs">{collection.collectionCount}</p>
+                <Link key={collection.id} to={`/collections/${collection.id}`}>
+                  <div className="flex flex-col gap-1">
+                    <div className="overflow-hidden rounded-lg relative aspect-square">
+                      <div className="absolute top-1 left-1 surface-material px-2 py-1 rounded-md">
+                        <p className="text-xs">{collection.collectionCount}</p>
+                      </div>
+
+                      <img className="w-full h-full object-cover" src={collection.thumbnail} />
                     </div>
 
-                    <img className="w-full object-cover" src={collection.thumbnail} />
+                    <p className="text-xs text-02">{collection.name}</p>
                   </div>
-
-                  <p className="text-xs text-02">{collection.name}</p>
-                </div>
+                </Link>
               ))}
             </div>
           </div>
