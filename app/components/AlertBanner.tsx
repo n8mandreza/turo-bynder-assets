aac9fd31c90e351004a5a1b9f007fc2f0b4610dc
Nathan Mandreza <nymandreza@gmail.com>
2024-07-14 19:06:02 -0700

    Update AlertBanner style

--- app/components/AlertBanner.tsx
+++ app/components/AlertBanner.tsx
@@ -7,14 +7,14 @@ interface AlertBannerProps {
 
 export default function AlertBanner({title, message}: AlertBannerProps) {
     return (
-        <div className="surface-alert flex gap-3 items-center p-4">
+        <div className="surface-alert flex gap-3 items-center p-4 rounded-xl">
             <div className="icon-alert w-6 h-6">
                 <AlertFilled />
             </div>
 
             <div className="flex flex-col gap-1">
                 {title ? (
-                    <h4 className="font-black">{title}</h4>
+                    <h4 className="font-bold">{title}</h4>
                 ) : null}
 
                 <p className="text-sm">{message}</p>
